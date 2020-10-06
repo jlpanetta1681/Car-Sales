a83e81517c8d1f78d9a3860a3a982b4354d0b3ef
Joe-Panetta <jlpanetta1681@icloud.com>
2020-10-06 10:57:27 -0400

    set up imports and provider for redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { createStore } from 'redux';
 import 'bulma/css/bulma.css';
 import './styles.scss';
+import { Provider } from 'react-redux';
+function reducer() {
+	return {
+		title: 'Hi from the redux store',
+	};
+}
+
+const store = createStore(reducer);
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	rootElement
+);
